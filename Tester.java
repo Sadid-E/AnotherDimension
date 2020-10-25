53c4c01805f57fd37ca8b2ea60deebdcccd9eb50
Sadid-E <sethun20@stuy.edu>
2020-10-25 13:36:12 -0400

    Added sum() and tests for sum()

---
+++ Tester.java
@@ -0,0 +1,10 @@
+public class Tester {
+
+public static void main(String[] args) {
+  int[] arr = {3,2,8,6};
+  int[] arr2 = {};
+  System.out.println(ArrayOps.sum(arr)==19);
+  System.out.println(ArrayOps.sum(arr2)==0);
+}
+
+}
